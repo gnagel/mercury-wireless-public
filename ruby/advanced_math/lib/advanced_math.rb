c674874af511fb3abb160719025bbb558b646410
gnagel <glenn@mercury-wireless.com>
2011-08-29 18:27:44 -0400

    Working on rsi gem

--- ruby/advanced_math/lib/advanced_math.rb
+++ ruby/advanced_math/lib/advanced_math.rb
@@ -1,5 +1,5 @@
 module AdvancedMath
-  VERSION = '0.0.7'
+  VERSION = '0.0.8'
 end
 
 require File.dirname(__FILE__) + "/advanced_math/sma.rb"
--- ruby/advanced_math/lib/advanced_math/rsi.rb
+++ ruby/advanced_math/lib/advanced_math/rsi.rb
@@ -6,12 +6,15 @@ module AdvancedMath
   # Created: 2011-08-28
   # Author:  G Nagel
   # Company: Mercury Wireless Software LLC
+  #
+  # Source: http://www.tradingmarkets.com/.site/stocks/commentary/editorial/2-Period-RSI.cfm
+  #
   # Source: http://en.wikipedia.org/wiki/Relative_Strength_Index#Cutler.27s_RSI
   # Source: http://www.aspenres.com/Documents/AspenGraphics4.0/Aspen_Graphics_4.htm#CutlersRSI.htm
   # Source: http://stockcharts.com/school/doku.php?id=chart_school:technical_indicators:relative_strength_index_rsi
   ###
   class RelativeStrengthIndex
-    attr_accessor :values_up, :values_down, :range, :last_value
+    attr_accessor :values_up, :values_down, :range, :last_value, :count
     attr_accessor :verbose
     ###
     # Initialize the members
@@ -25,6 +28,7 @@ module AdvancedMath
       @values_down = []
       @range = range.to_i()
       @last_value = nil
+      @count = 0
     end
 
     ###
@@ -49,14 +53,14 @@ module AdvancedMath
       diff = value.to_f - @last_value
       @last_value = value.to_f()
       if (0 > diff)
-      @values_up << 0
-      @values_down << diff.abs()
+        @values_up << 0
+        @values_down << diff.abs()
       elsif (0 < diff)
-      @values_up << diff
-      @values_down << 0
+        @values_up << diff
+        @values_down << 0
       else
-      @values_up << 0
-      @values_down << 0
+        @values_up << 0
+        @values_down << 0
       end
       puts "diff = #{diff}, values_up = #{@values_up.inspect()}, values_down = #{@values_down.inspect()}" if verbose()
 
@@ -73,8 +77,17 @@ module AdvancedMath
       end
 
       sum = {:up => 0, :down => 0, :i_up => 0, :i_down => 0}
-      @values_up.each()   { |value| sum[:up]   = sum[:up] + value }
-      @values_down.each() { |value| sum[:down] = sum[:down] + value }
+      
+      @values_up.slice(0, range()-1).each()   { |value| sum[:up]   = sum[:up] + value }
+      @values_down.slice(0, range()-1).each() { |value| sum[:down] = sum[:down] + value }
+      if (count() == 0)
+        sum[:up]   = sum[:up] + @values_up[range()-1]
+        sum[:down] = sum[:down] + @values_down[range()-1]
+      else
+        sum[:up] = sum[:up]/(range()-1) + @values_up[range()-1]
+        sum[:down] = sum[:down]/(range()-1) + @values_down[range()-1]
+      end
+      @count = count() + 1
 
       rs = (0 == sum[:down]) ? 100.0 : (sum[:up] / sum[:down])
       puts "rs = #{rs}" if verbose()
