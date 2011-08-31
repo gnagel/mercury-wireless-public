48795317717045670325757a246d37ff361b39f3
gnagel <glenn@mercury-wireless.com>
2011-08-31 14:57:59 -0400

    Awesomeness abounds! We now have a horizontal sliding on/off button

---
+++ android/SlidingButton/src/com/mercury_wireless/android/demo/sliding_button/SlidingButtonAnimation.java
@@ -0,0 +1,84 @@
+package com.mercury_wireless.android.demo.sliding_button;
+
+
+import android.graphics.Camera;
+import android.graphics.Matrix;
+import android.view.View;
+import android.view.animation.AccelerateInterpolator;
+import android.view.animation.Animation;
+import android.view.animation.RotateAnimation;
+import android.view.animation.Transformation;
+
+
+public class SlidingButtonAnimation extends Animation {
+	public static void rotate(final View view, final float start, final float end) {
+		// Find the center of image
+		final float centerX = view.getWidth() / 2.0f;
+		final float centerY = view.getHeight() / 2.0f;
+
+		// Create a new 3D rotation with the supplied parameter
+		// The animation listener is used to trigger the next animation
+		final android.view.animation.RotateAnimation rotation = new RotateAnimation(start, end, centerX, centerY);
+
+		// final SlidingButtonAnimation rotation = new
+		// SlidingButtonAnimation(start, end, centerX, centerY);
+		rotation.setDuration(500);
+		rotation.setFillAfter(true);
+		rotation.setInterpolator(new AccelerateInterpolator());
+		// rotation.setAnimationListener(new DisplayNextView(isFirstImage,
+		// image1, image2));
+
+		view.startAnimation(rotation);
+	}
+
+
+	private final float	mFromDegrees;
+
+	private final float	mToDegrees;
+
+	private final float	mCenterX;
+
+	private final float	mCenterY;
+
+	private Camera		mCamera;
+
+
+	public SlidingButtonAnimation(final float fromDegrees, final float toDegrees, final float centerX,
+			final float centerY) {
+		mFromDegrees = fromDegrees;
+		mToDegrees = toDegrees;
+		mCenterX = centerX;
+		mCenterY = centerY;
+	}
+
+
+	@Override
+	protected void applyTransformation(final float interpolatedTime, final Transformation t) {
+		final float fromDegrees = mFromDegrees;
+		final float degrees = fromDegrees + (mToDegrees - fromDegrees) * interpolatedTime;
+
+		final float centerX = mCenterX;
+		final float centerY = mCenterY;
+		final Camera camera = mCamera;
+
+		final Matrix matrix = t.getMatrix();
+
+		camera.save();
+
+		camera.rotateY(degrees);
+
+		camera.getMatrix(matrix);
+		camera.restore();
+
+		matrix.preTranslate(-centerX, -centerY);
+		matrix.postTranslate(centerX, centerY);
+
+	}
+
+
+	@Override
+	public void initialize(final int width, final int height, final int parentWidth, final int parentHeight) {
+		super.initialize(width, height, parentWidth, parentHeight);
+		mCamera = new Camera();
+	}
+}
--- android/SlidingButton/src/com/mercury_wireless/android/demo/sliding_button/SlidingHorizontalButton.java
+++ android/SlidingButton/src/com/mercury_wireless/android/demo/sliding_button/SlidingHorizontalButton.java
@@ -4,6 +4,8 @@ package com.mercury_wireless.android.demo.sliding_button;
 import android.content.Context;
 import android.util.AttributeSet;
 import android.view.View;
+import android.view.animation.AccelerateInterpolator;
+import android.view.animation.TranslateAnimation;
 import android.widget.LinearLayout;
 import android.widget.ViewFlipper;
 
@@ -15,6 +17,10 @@ public class SlidingHorizontalButton extends LinearLayout implements View.OnClic
 	private View		viewLeft;
 
 	private View		viewRight;
+
+	private View		view;
+
+	private boolean		checked	= true;
 
 
 	public SlidingHorizontalButton(final Context context) {
@@ -52,6 +58,37 @@ public class SlidingHorizontalButton extends LinearLayout implements View.OnClic
 				viewFlipper.setOutAnimation(context, R.anim.toggle_button_transition_out_left);
 				viewFlipper.showPrevious();
 				break;
+
+			case R.id.container:
+			case R.id.button:
+				// final View view = (View) this.view.getParent();
+
+				// Find the center of image
+				final float centerX = view.getWidth() / 2.0f;
+				final float centerY = view.getHeight() / 2.0f;
+
+				// Create a new 3D rotation with the supplied parameter
+				// The animation listener is used to trigger the next animation
+				final android.view.animation.TranslateAnimation rotation = checked ? new TranslateAnimation(0F,
+						-view.getWidth(), 0F, 0F) : new TranslateAnimation(-view.getWidth(), 0F, 0F, 0F);
+
+				// final SlidingButtonAnimation rotation = new
+				// SlidingButtonAnimation(start, end, centerX, centerY);
+				rotation.setDuration(200);
+				rotation.setFillAfter(true);
+				rotation.setInterpolator(new AccelerateInterpolator());
+				// rotation.setAnimationListener(new
+				// DisplayNextView(isFirstImage,
+				// image1, image2));
+
+				view.startAnimation(rotation);
+
+				// SlidingButtonAnimation.rotate(view, 0, -180);
+
+				checked = !checked;
+
+				// view.startAnimation(AnimationUtils.loadAnimation(context,
+				// R.anim.toggle_button_rotate));
 		}
 	}
 
@@ -72,6 +109,12 @@ public class SlidingHorizontalButton extends LinearLayout implements View.OnClic
 		container.setOnClickListener(this);
 		viewRight = container.findViewById(R.id.button_on);
 		viewRight.setOnClickListener(this);
+
+		container = root.findViewById(R.id.container);
+		container.setOnClickListener(this);
+
+		view = container.findViewById(R.id.button);
+		view.setOnClickListener(this);
 
 		return root;
 	}
